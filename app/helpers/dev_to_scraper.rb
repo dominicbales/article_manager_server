75b6c954d08e5dd13445818f136a253d169a3783
Dominic Bales <dominic.bales@madeintandem.com>
2020-05-06 17:17:27 -0500

    Create the api to add new articles

--- app/helpers/dev_to_scraper.rb
+++ app/helpers/dev_to_scraper.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 class DevToScraper < ArticleDataFetcher
-  def grab
+  def grab(account)
     puts 'inside devToScrape'
     @doc = Nokogiri::HTML(URI.open('https://dev.to/t/javascript'))
     @titles = []
@@ -12,7 +12,7 @@ class DevToScraper < ArticleDataFetcher
       @urls = link.css('.index-article-link')[0]['href']
       @avatar = link.css('.small-pic').css('img').attr('src').text
       @read_time = link.css('.article-reading-time').text
-      @titles.push({title: link.css('a').css('.content').css('h3').text, link: @urls, avatar: @avatar, time: @read_time}) 
+      @titles.push({title: link.css('a').css('.content').css('h3').text, link: @urls, avatar: @avatar, time: @read_time, account_id: account.id}) 
     end
     @titles
   end
