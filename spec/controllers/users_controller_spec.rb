a5a121bb1b013971f50e929568e8b115425988b0
Dominic Bales <dominic.bales@madeintandem.com>
2020-06-12 15:28:01 -0500

    changed user_params name in user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,22 +8,22 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     let(:user) {create(:user)}
 
     describe "GET users#index" do
-      let(:user_params) { FactoryBot.attributes_for(:user) }
+      let(:user_params_mock) { FactoryBot.attributes_for(:user) }
 
       it "returns all users" do 
-        test_user = User.create(user_params)
+        test_user = User.create(user_params_mock)
         get :index
         expect(JSON.parse(response.body).length).to eq(1)
       end
     end
     
     describe "POST users#create" do
-      let(:user_params) { FactoryBot.attributes_for(:user) }
+      let(:user_params_mock) { FactoryBot.attributes_for(:user) }
 
       it "creates a user" do
-        post :create, params: { user: user_params }
+        post :create, params: { user: user_params_mock }
         test_user = JSON.parse(response.body)
-        expect(test_user['id']).to eq(user_params[:id])        
+        expect(test_user['id']).to eq(user_params_mock[:id])        
       end
     end
 end
