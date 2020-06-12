bcbc6151fe413d162ff026c47485d7382c0e6ad7
Dominic Bales <dominic.bales@madeintandem.com>
2020-06-12 15:01:06 -0500

    setup rspec and database_cleaner, also added user controller spec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,10 +1,12 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "spec_helper"
 
 # Change this ArticlesController to your project
-RSpec.describe ArticlesController, type: :controller do
+describe Api::V1::ArticlesController do
     describe "GET articles#index" do
       it "return all articles" do
-        get '/'
+        puts 'test articles'
       end
     end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,29 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "spec_helper"
+require "rails_helper"
 
 # Change this ArticlesController to your project
-RSpec.describe UsersController, type: :controller do
+RSpec.describe Api::V1::UsersController, type: :controller do 
+    let(:user) {create(:user)}
+
     describe "GET users#index" do
-      it "return all users" do
-        get '/'
+      let(:user_params) { FactoryBot.attributes_for(:user) }
+
+      it "returns all users" do 
+        test_user = User.create(user_params)
+        get :index
+        expect(JSON.parse(response.body).length).to eq(1)
+      end
+    end
+    
+    describe "POST users#create" do
+      let(:user_params) { FactoryBot.attributes_for(:user) }
+
+      it "creates a user" do
+        post :create, params: { user: user_params }
+        test_user = JSON.parse(response.body)
+        expect(test_user['id']).to eq(user_params[:id])        
       end
     end
 end
